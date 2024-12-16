6bdd1b718fdf3bf27f05cd5ac3ea2eb367b2e38d
root <root@ip-172-31-17-42.ap-southeast-1.compute.internal>
2024-12-16 06:40:27 +0000

    prod

--- config/livewire.php
+++ config/livewire.php
@@ -65,26 +65,13 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules'  => ['file', 'max:102400'],        // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
-            'png',
-            'gif',
-            'bmp',
-            'svg',
-            'wav',
-            'mp4',
-            'mov',
-            'avi',
-            'wmv',
-            'mp3',
-            'm4a',
-            'jpg',
-            'jpeg',
-            'mpga',
-            'webp',
-            'wma',
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
         'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
@@ -168,7 +155,7 @@ return [
     | on the page. If you want Bootstrap CSS, you can specify: "bootstrap"
     |
     */
-    'debug' => true,
 
     'pagination_theme' => 'bootstrap',
 ];
+
