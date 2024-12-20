b41019ee094b0167ee033dc9adeb3064cfa18443
EJ <124268472+Errol13@users.noreply.github.com>
2024-12-20 14:47:37 +0800

    Update client-home.blade.php

--- resources/views/livewire/client-home.blade.php
+++ resources/views/livewire/client-home.blade.php
@@ -75,7 +75,9 @@
                 }
                 }
                 @endphp
+                @if($relativePath)
                 <img src="{{ asset('storage/' . $relativePath) }}" class="rounded-0" alt="cover" style="border-radius: 15px 15px 0 0; width: 100%; height: 150px; object-fit: cover;">
+             @endif
                 @else
                 <img src="{{ asset('assets/cover_def.png') }}" class="rounded-0" alt="cover" style="border-radius: 15px 15px 0 0; width: 100%; height: 150px; object-fit: cover;">
                 @endif
